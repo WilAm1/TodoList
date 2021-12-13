d07e506a3504cd5686308c833edccb952e9f0529
Nub <wilhelmjanmapogi@gmail.com>
2021-12-13 15:24:16 +0800

    Fixed static variables

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,23 +1,25 @@
 export default class {
-    subscribers = {};
+    static subscribers = {};
+    constructor() {
 
+    }
     static subscribe(topic, cb) {
-        if (!Array.isArray(subscribers[topic])) {
-            subscribers[topic] = [];
+        if (!Array.isArray(this.subscribers[topic])) {
+            this.subscribers[topic] = [];
         }
-        subscribers[topic].push(cb);
-        const index = subscribers[topic].length - 1;
+        this.subscribers[topic].push(cb);
+        const index = this.subscribers[topic].length - 1;
         return {
             unsubscribe() {
-                subscribers[topic].splice(index, 1);
+                this.subscribers[topic].splice(index, 1);
             }
         }
     }
     static publish(topic, data) {
-        if (!Array.isArray(subscribers[topic])) {
+        if (!Array.isArray(this.subscribers[topic])) {
             return
         }
-        subscribers[topic].forEach(callback => {
+        this.subscribers[topic].forEach(callback => {
             callback(data)
         });
     }
