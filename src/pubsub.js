c2da4da8ee60fc9ddc0489c2604b3afe9bc7e2c8
Nub <wilhelmjanmapogi@gmail.com>
2021-12-13 11:09:35 +0800

    Add pubsub module

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,5 +1,24 @@
 export default class {
-    constructor() {
-        this.subscribers = {};
+    subscribers = {};
+
+    static subscribe(topic, cb) {
+        if (!Array.isArray(subscribers[topic])) {
+            subscribers[topic] = [];
+        }
+        subscribers[topic].push(cb);
+        const index = subscribers[topic].length - 1;
+        return {
+            unsubscribe() {
+                subscribers[topic].splice(index, 1);
+            }
+        }
+    }
+    static publish(topic, data) {
+        if (!Array.isArray(subscribers[topic])) {
+            return
+        }
+        subscribers[topic].forEach(callback => {
+            callback(data)
+        });
     }
 }
